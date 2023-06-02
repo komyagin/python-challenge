a8ad254ce63bea1abd7f02b124f147fcafb8ba74
Semyon Komyagin <komyagin.s@gmail.com>
2023-06-03 00:33:15 +0500

    Day 16 - OOP Coffee Machine, Pretty Table

--- day16/main.py
+++ day16/main.py
@@ -1,69 +1,27 @@
-from data import MENU
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 from art import logo
-from prettytable import PrettyTable
 
-profit = 0
-resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-}
-
-
-def is_resource_sufficient(order_ingredients):
-    for item in order_ingredients:
-        if order_ingredients[item] > resources[item]:
-            print(f"Sorry there is not enough {item}.")
-            return False
-    return True
-
-
-def process_coins():
-    print("Please insert coins.")
-    total = int(input("how many quarters?: ")) * 0.25
-    total += int(input("how many dimes?: ")) * 0.1
-    total += int(input("how many nickles?: ")) * 0.05
-    total += int(input("how many pennies?: ")) * 0.01
-    return total
-
-
-def is_transaction_successful(money_received, drink_cost):
-    if money_received >= drink_cost:
-        change = round(money_received - drink_cost, 2)
-        print(f"Here is ${change} in change.")
-        global profit
-        profit += drink_cost
-        return True
-    else:
-        print("Sorry that's not enough money. Money refunded.")
-        return False
-
-
-def make_coffee(drink_name, order_ingredients):
-    for item in order_ingredients:
-        resources[item] -= order_ingredients[item]
-    print(f"Here is your {drink_name} ☕️. Enjoy!")
-
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
 
 is_on = True
+print("\n@halmeru Day 16 #100DaysOfCode")
+print(logo)
 
 while is_on:
-    print("@halmeru Day 16 #100DaysOfCode")
-    print(logo)
-    choice = input("What would you like? (espresso/latte/cappuccino): ")
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}) ")
     if choice == "off":
         is_on = False
     elif choice == "report":
-        table = PrettyTable()
-        table.field_names = ["Name", "Resource", "Measure"]
-        table.add_row(["Water", resources['water'], 'ml'])
-        table.add_row(["Milk", resources['milk'], "ml"])
-        table.add_row(["Coffee", resources['coffee'],"g"])
-        table.add_row(["Money", profit, "$"])
-        print(table)
+        coffee_maker.report()
+        money_machine.report()
     else:
-        drink = MENU[choice]
-        if is_resource_sufficient(drink["ingredients"]):
-            payment = process_coins()
-            if is_transaction_successful(payment, drink["cost"]):
-                make_coffee(choice, drink["ingredients"])
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
+
+
